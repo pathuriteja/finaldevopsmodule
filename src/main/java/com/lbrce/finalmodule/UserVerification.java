456e66c850ff27403ccd6293e890573cb8df692a
pathuriteja <110657770+pathuriteja@users.noreply.github.com>
2023-10-11 15:01:39 +0530

    Add files via upload

---
+++ src/main/java/com/lbrce/finalmodule/UserVerification.java
@@ -0,0 +1,14 @@
+package com.lbrce.finalmodule;
+import java.util.ResourceBundle;
+public class UserVerification {
+	public boolean check(String uname,String pswd)
+	{
+		ResourceBundle rb=ResourceBundle.getBundle("config");
+		String username=rb.getString("uname");
+		String password=rb.getString("password");
+		if(uname.equals(username)&&pswd.equals(password))
+		        	return true;
+		else
+					return false;
+	}
+}
